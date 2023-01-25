acd8ae4cb68899d5eef71f23738825f50ac8d10c
Ruban Manoj Paul <ruban-manoj-paul@myseneca.ca>
2023-01-25 01:47:25 -0500

    added jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
+// .eslintrc.js
+
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+    env: {
+      node: true,
+      commonjs: true,
+      es2021: true,
+      // Add this next line to configure ESLint for Jest, see:
+      // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+      jest: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 13,
     },
-    "rules": {
-    }
-}
+    rules: {},
+  };
