d177e2a8c4f0c12d133210456a9646076a5b566f
Ruban Manoj Paul <ruban-manoj-paul@myseneca.ca>
2023-03-24 20:42:28 -0400

    lab8 added

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -7,18 +7,6 @@
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 
-/*module.exports = (req, res) => {
-  if (req.headers.authorization) {
-    res.status(200).json(createSuccessResponse({
-      status: 'ok',
-      fragments: [],
-    }));
-  }
-   else if (!req.headers.authorization) {
-    res.status(401).json(createErrorResponse(401, 'Unauthorized'));
-  }  
-  };*/
-
   module.exports = async (req, res) => {
     try {
       res.status(200).json(
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,29 +1,18 @@
 // src/routes/api/post.js
 const { createSuccessResponse, createErrorResponse } = require('../../response');
-
 const { Fragment } = require('../../../src/model/fragment');
-
-const contentType = require('content-type');
-
 const logger = require('../../logger');
-
 const apiURL = process.env.API_URL || process;
-
 //Creates a new fragment for the current (i.e., authenticated user)
 module.exports = async (req, res) => {
-  const { type } = contentType.parse(req);
-  var supported = Fragment.isSupportedType(type);
-
+  var supported = Fragment.isSupportedType(req.get('Content-Type'));
   if (supported) {
     try {
       ///generate a new fragment metadata record for the data
-      const fragment = new Fragment({ ownerId: req.user, type: type });
-      //console.log(fragment);
-      //both the data and metadata are stored
+      const fragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type') });
       logger.info('saving fragment data');
       await fragment.setData(req.body);
       fragment.save();
-      logger.info('getting saved fragment by id');
       res.setHeader('Content-type', fragment.type);
       res.setHeader('Location', apiURL + '/v1/fragments/' + fragment.id);
       res.status(201).json(
